6c4f93f42e930836807d28556cdde37c8b91f532
Oladoyinbo Vincent <oladoyinboadverts@gmail.com>
2023-11-15 09:11:39 -0800

    :memo: wip

---
+++ src/Console/Terminal.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FastVolt\Utils\Console;
+
+class Terminal 
+{
+
+    public function __construct(
+        private array $commands
+    ) {
+        //
+    }
+
+
+    public function checkDir()
+    {
+        if (is_array($this->commands)) {
+           
+        }
+    }
+}
